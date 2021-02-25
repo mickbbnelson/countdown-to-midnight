cc565001cea135fba68b74456d3b348a18e6faa6
Mick Nelson <mickbbnelson@gmail.com>
2021-02-25 07:23:34 -0800

    done

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(number)
+    while number > 0
+        puts "#{number} SECOND(S)!"
+        number -= 1     #why does the order of these two lines in the block matter?
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(number)
+    while number > 0
+        puts "#{number} SECOND(S)!"
+        number -= 1  
+        sleep(1)   
+    end
+    "HAPPY NEW YEAR!"
 end
